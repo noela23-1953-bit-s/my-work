2f7c4bb1fe59be302cda868737fae9a581677ccd
noela23-1953-bit-s <gracianoela2@gmail.com>
2025-02-01 16:16:01 +0300

    commit 11
    in the NV on line 11, a tab was added.

--- laticia/employee/src/Main.java
+++ laticia/employee/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Customer cus = new Customer("Cash money", "NSSF Account", 700.0);
 
 
-        System.out.println("The Customers name is : " + cus.getName());
+        System.out.println("The Customers name is : " + cus.get Name());
         System.out.println("The Accounts name is : " + cus.getAccountName());
         System.out.println("The Balance left is : UGX " + cus.getBalance());
 
