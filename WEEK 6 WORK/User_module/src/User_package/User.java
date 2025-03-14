1c76a8085e4df87229a567554d5cb9ee5bf9eb80
noela23-1953-bit-s <gracianoela2@gmail.com>
2025-03-14 17:33:25 +0300

    commit#2
    in the NV on line 5 a tab was added.

--- WEEK 6 WORK/User_module/src/User_package/User.java
+++ WEEK 6 WORK/User_module/src/User_package/User.java
@@ -1,8 +1,8 @@
 package User_package;
 
 public class User {
-    String  name;
-    String Address;
+    String name;
+    String  Address;
    private String Amountpaid;
 
     public User(String name, String address, int age) {
